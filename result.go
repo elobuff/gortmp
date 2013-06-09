a80589b33050300f8a1313acf4ed2da763921f9b
Jason Coene <jcoene@gmail.com>
2013-06-09 16:19:56 -0500

    Add error decoding and dump helpers

--- result.go
+++ result.go
@@ -1,9 +1,20 @@
 package rtmp
 
+import (
+	"github.com/elobuff/goamf"
+)
+
 type Result struct {
 	Name          string
 	TransactionId float64
 	Objects       []interface{}
+}
+
+type ResultError struct {
+	Type         string
+	ErrorCode    string
+	Message      string
+	Substitution string
 }
 
 func (r *Result) IsResult() bool {
@@ -12,4 +23,40 @@ func (r *Result) IsResult() bool {
 
 func (r *Result) IsError() bool {
 	return r.Name == "_error"
+}
+
+func (r *Result) DecodeError() (result ResultError, err error) {
+	for _, obj := range r.Objects {
+		if tobj, ok := obj.(amf.TypedObject); ok == true {
+			if tobj.Type == "flex.messaging.messages.ErrorMessage" {
+				if rc := tobj.Object["rootCause"]; rc != nil {
+					if rootCause, ok := rc.(amf.TypedObject); ok == true {
+						result = *new(ResultError)
+						result.Type = rootCause.Type
+
+						if tmp, ok := rootCause.Object["errorCode"].(string); ok {
+							result.ErrorCode = tmp
+						}
+
+						if tmp, ok := rootCause.Object["message"].(string); ok {
+							result.Message = tmp
+						}
+
+						if sa := rootCause.Object["substitutionArguments"]; sa != nil {
+							if subArgs, ok := sa.(amf.Array); ok {
+								len := len(subArgs)
+								if tmp, ok := subArgs[len-1].(string); ok {
+									result.Substitution = tmp
+								}
+							}
+						}
+
+						return
+					}
+				}
+			}
+		}
+	}
+
+	return result, Error("Could not decode object error")
 }
--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package rtmp
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/jcoene/gologger"
@@ -8,6 +9,24 @@ import (
 )
 
 var log logger.Logger = *logger.NewLogger(logger.LOG_LEVEL_DEBUG, "rtmp")
+
+func DumpBytes(label string, buf []byte, size int) {
+	fmt.Printf("Dumping %s (%d bytes):\n", label, size)
+	for i := 0; i < size; i++ {
+		fmt.Printf("0x%02x ", buf[i])
+	}
+	fmt.Printf("\n")
+}
+
+func Dump(label string, val interface{}) error {
+	json, err := json.MarshalIndent(val, "", "  ")
+	if err != nil {
+		return Error("Error dumping %s: %s", label, err)
+	}
+
+	fmt.Printf("Dumping %s:\n%s\n", label, json)
+	return nil
+}
 
 func Error(f string, v ...interface{}) error {
 	return errors.New(fmt.Sprintf(f, v...))
