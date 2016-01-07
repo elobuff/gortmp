af7e53107f0dc38d219bb40436076da3f014efbd
Norwack <norwack@tuta.io>
2016-01-07 17:25:09 +0100

    update import path

--- client_invoke.go
+++ client_invoke.go
@@ -2,8 +2,9 @@ package rtmp
 
 import (
 	"bytes"
-	"code.google.com/p/go-uuid/uuid"
+
 	"github.com/elobuff/goamf"
+	"github.com/pborman/uuid"
 )
 
 func (c *Client) EncodeInvokeCommand(destination interface{}, operation interface{}, body interface{}) (msg *Message, err error) {
